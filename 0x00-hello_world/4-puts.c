225ac18a14e5f2f71ab02726d49a29197b38c363
awangui <86366610+awangui@users.noreply.github.com>
2022-09-08 12:47:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(void) /*main function*/
+/**
+ *main - entry point
+ *return: 0 (Success)
+ */
+int main(void) 
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
