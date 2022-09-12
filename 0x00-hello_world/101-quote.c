9cd67bd3eb0bf82ee56e11ba9b142e516694ade8
awangui <86366610+awangui@users.noreply.github.com>
2022-09-12 06:22:38 -0700

    task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - main block
  *
- * return: 1 (success)
+ * Return: 1 (success)
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
