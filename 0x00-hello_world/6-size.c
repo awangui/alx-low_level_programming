ee849b13ec919645c8a91127123b1831d5af7365
awangui <86366610+awangui@users.noreply.github.com>
2022-09-12 05:47:07 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", (int) sizeof(charType));
-printf("Size of an int: %d byte(s)\n", (int) sizeof(intType));
-printf("Size of a long int: %d byte(s)\n", (int) sizeof(longintType));
-printf("Size of a float: %d byte(s)\n", (int) sizeof(floatType));
+printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int) sizeof(longint));
+printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 return (0);
 }
