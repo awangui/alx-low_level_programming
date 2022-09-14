c3be31e373aae1af67ca5b004da65eb0b9178e20
awangui <86366610+awangui@users.noreply.github.com>
2022-09-14 14:56:39 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ * @n - number
+ *
+ * Return: mod (last digit)
+ */
+int print_last_digit(int n)
+{
+	int mod;
+
+	mod = n % 10;
+	if (n < 0)
+	{
+		mod = mod * -1;
+	}
+	mod = (mod + '0');
+	return (mod);
+}
