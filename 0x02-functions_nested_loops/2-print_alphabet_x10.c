3aa292d5a5476e2f81283a33b8737520b72d276f
awangui <86366610+awangui@users.noreply.github.com>
2022-09-14 14:09:24 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ **/
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	int letter = 'a';
+
+	while (num < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		putchar('\n');
+		num++;
+	}
+}
