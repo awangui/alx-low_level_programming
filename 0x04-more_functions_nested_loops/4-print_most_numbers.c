e3d6050a6dd3528ee1f0e16dd28169e2e9520c36
awangui <86366610+awangui@users.noreply.github.com>
2022-09-16 12:52:33 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' && i != '4')
+			_putchar('i');
+
+		i++;
+	}
+	_putchar('\n');
+}
