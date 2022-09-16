178b7bb68bd76c219deb91ea49d7614e5e719f63
awangui <86366610+awangui@users.noreply.github.com>
2022-09-16 03:07:27 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: 0 always
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	_putchar(product);
+	return (0);
+}
+
