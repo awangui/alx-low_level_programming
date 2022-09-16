a5d04c472bf4b482e3709b2a0e9b12019a88edbd
awangui <86366610+awangui@users.noreply.github.com>
2022-09-16 12:57:19 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_most_numbers - prints the numbers, from 0 to 9
  *
  * Return: void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 0;
 
