6a25b7e4421de01637149eada7bf6f288e91bb1c
awangui <86366610+awangui@users.noreply.github.com>
2022-09-16 13:16:33 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int num = 0;
+
+	while (i <= 10)
+	{
+
+	while (i <= 14)
+	{
+		_putchar(i + '0');
+
+		i++;
+	}
+	_putchar('\n');
+	}
+	num++;
+}
