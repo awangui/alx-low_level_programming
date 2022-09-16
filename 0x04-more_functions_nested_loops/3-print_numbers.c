60817fd357ae8d06c9a30290f711f0da61d1d875
awangui <86366610+awangui@users.noreply.github.com>
2022-09-16 03:17:01 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
