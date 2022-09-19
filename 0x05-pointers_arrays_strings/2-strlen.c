abd214233f89509987766b9f9e3325f796bdb78d
awangui <86366610+awangui@users.noreply.github.com>
2022-09-19 12:23:15 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: unassigned char
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+
