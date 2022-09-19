c29bbd59c8897619aabf5bea025c236a60a35280
Anita Wangui <86366610+awangui@users.noreply.github.com>
2022-09-19 23:42:13 +0300

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /** 
- * puts2 - prints characters of a string starting with 
+ * puts2 - prints characters of a string starting with
  * the first character
- * @str: string
+ * @str: the string whereby the characters are pribted individually
  */
- void puts2(char *str){	int n = 0; 	while (*(str + n))	
-{
+
+void puts2(char *str){
+	int n = 0;
+
+ 	while (*(str + n))
+	{
 		putchar(*(str + n));
 		n = n + 2;
-}
-                putchar('\n');
+	}
+ 	putchar('\n');
 }
