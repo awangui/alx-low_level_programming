0d518dcef4cbd54e454b3caabd0c39cd85ce8ade
Anita Wangui <86366610+awangui@users.noreply.github.com>
2022-09-19 23:37:46 +0300

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/** 
+ * puts2 - prints characters of a string starting with 
+ * the first character
+ * @str: string
+ */
+ void puts2(char *str){	int n = 0; 	while (*(str + n))	
+{
+		putchar(*(str + n));
+		n = n + 2;
+}
+                putchar('\n');
+}
