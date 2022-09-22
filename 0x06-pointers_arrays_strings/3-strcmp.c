0bb1f4ce57c87e058671c364b80a0ebd2bbb9efe
awangui <86366610+awangui@users.noreply.github.com>
2022-09-22 11:12:06 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp: a function that compares two strings
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: a function that compares two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\o'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (op);
+}
