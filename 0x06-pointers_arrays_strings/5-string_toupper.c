067426e4867fd03eae4bc228b63e1db4436d8e25
awangui <86366610+awangui@users.noreply.github.com>
2022-09-23 13:54:07 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * string_toupper - function that changes all lowercase
+ * letters of a string to uppercase
+ * @s: param
+ *
+ * Return: 0 always
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
