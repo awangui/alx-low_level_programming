3375cfae21a26a2cb9fc5c123d3f92220efa73b0
awangui <86366610+awangui@users.noreply.github.com>
2022-09-26 11:42:15 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * _memset -  function that fills memory with a constant byte
  * @s: char s
@@ -11,7 +11,8 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	uunsigned int i;
+	unsigned int i;
+
 	for (i = 0; i < n; i++)
 		*(s + i) = b;
 	return (s);
