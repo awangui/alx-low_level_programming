fe2660b068583b5232a3e090ca354846c0d3468b
awangui <86366610+awangui@users.noreply.github.com>
2022-10-06 03:18:29 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -22,7 +22,7 @@ int largest_number(int a, int b, int c)
 
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 
 	{
 
@@ -30,7 +30,7 @@ int largest_number(int a, int b, int c)
 
 	}
 
-	else if (b > c)
+	else if (b >= a && b >= c)
 
 	{
 
