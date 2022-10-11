9d0ec565ba5e99e4dbf55cbdfa94bb5a377f45bd
awangui <86366610+awangui@users.noreply.github.com>
2022-10-11 12:48:35 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initialzes a variale of type struct dog
+ * @d: pointer
+ * @name: name to initialize
+ * @age: `` ```
+ * @owner: ``
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = "fluffy";
+	d->age = 2;
+	d->owner = "Wangui";
+}
