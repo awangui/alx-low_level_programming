2840e7f973c3e1a93e4c2b76d56092cbdd57bc96
awangui <86366610+awangui@users.noreply.github.com>
2022-10-11 05:38:54 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - function that prints a string,
  * followed by a new line.
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * is_prime_number - prints 1 if the input integer is a prime number
+ * @n: integer to be checked
+ *
+ * Return: 1 if number is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 1
+
