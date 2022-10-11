cda0c660dfc441718f576ae69c774e7098f84bb7
awangui <86366610+awangui@users.noreply.github.com>
2022-10-11 12:33:10 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog's info
+ * @name: element one
+ * @age: element two
+ * @owner: element three
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
