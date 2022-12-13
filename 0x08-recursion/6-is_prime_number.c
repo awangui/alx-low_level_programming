23d34fc2541de472b7a7f718509f7b4e21e0aa46
awangui <86366610+awangui@users.noreply.github.com>
2022-12-12 23:07:08 -0800

    task six

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,15 +7,15 @@
  */
 int is_prime_number(int n)
 {
+	if (n == 1)
+	{
+		return (0);
+	}
 	if (n % 2 == 1)
 	{
 		return (1);
 	}
 	if (n % 2 == 0)
-	{
-		return (0);
-	}
-	if (n == 1)
 	{
 		return (0);
 	}
